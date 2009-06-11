3e053f85120f802fe9b07c8329b919bd46b11adb
Vladimir Hidalgo <vlad@todosv.com>
2009-06-11 16:26:07 -0600

    Retornar a la página que solicitó el login

--- PHP/inicio.php
+++ PHP/inicio.php
@@ -1,28 +1,34 @@
 <?php
 function CONTENIDO_INICIAR_SESION()
 {
-if (S_iniciado())
-{
-    header("location: ./");
-    return;
-}
-echo "¿Deseas comprar y vender pero aún no tienes una cuenta? ". ui_href("iniciar_sesion_crear_cuenta","./registrar","¡entonces registrate ahora!") . ", es gratis, fácil y rápido.<br />";
-/* Si ya intentó iniciar... */
+
 if (isset($_POST['iniciar_proceder']))
 {
     $ret = _F_usuario_acceder($_POST['iniciar_campo_correo'],$_POST['iniciar_campo_clave']);
-    // Si fue exitoso
-    if ($ret == 1)
-    {
-        header("location: ./");
-        return;
-    }
-    else
+
+    if ($ret != 1)
     {
         echo mensaje ("Datos de acceso erroneos, por favor intente de nuevo",_M_ERROR);
     }
 }
+
+if (S_iniciado())
+{
+    if (!empty($_POST['iniciar_retornar']))
+    {
+        header("location: ".$_POST['iniciar_retornar']);
+    }
+    else
+    {
+        header("location: ./");
+    }
+    return;
+}
+
+echo "¿Deseas comprar y vender pero aún no tienes una cuenta? ". ui_href("iniciar_sesion_crear_cuenta","./registrar","¡entonces registrate ahora!") . ", es gratis, fácil y rápido.<br />";
+$retorno = "http://".$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 echo "<form action=\"iniciar\" method=\"POST\">";
+echo ui_input("iniciar_retornar", $retorno, "hidden");
 echo "<table>";
 echo "<span class=\"explicacion\">Utilice el mismo correo y contraseña que utilizó al registrarse en el sistema</span>";
 echo ui_tr(ui_td("Correo electronico (e-mail)") . ui_td(ui_input("iniciar_campo_correo")));
