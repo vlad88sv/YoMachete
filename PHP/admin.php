1b09d8d87767b6dfed54e595ea6bc2cda3bc7cd5
Vladimir Hidalgo <vlad@todosv.com>
2009-06-11 17:35:19 -0600

    Soporte inicial para validar registros de usuarios.

--- PHP/admin.php
+++ PHP/admin.php
@@ -1,3 +1,35 @@
 <?php
+function CONTENIDO_ADMIN()
+{
+    if (empty($_GET['op']))
+    {
+        echo "<h1>Bienvenido a la interfaz de administración</h1>";
+        echo "Por favor seleccione el área a administrar:";
+        echo "<ul>";
+        echo "<li>".ui_href("admin_usuarios_activacion","admin_usuarios_activacion","Usuarios: activación de cuentas")."</li>";
+        echo "<li>".ui_href("admin_usuarios_admin","admin_usuarios_admin","Usuarios: administración general")."</li>";
+        echo "<li>".ui_href("admin_categorias_admin","admin_categorias_admin","Categorias: administración general")."</li>";
+        echo "<li>".ui_href("admin_articulos_admin","admin_categorias_admin","Publicaciones: aprobación")."</li>";
+        echo "<li>".ui_href("admin_articulos_admin","admin_categorias_admin","Publicaciones: administración general")."</li>";
+        echo "</ul>";
+        return;
+    }
 
+    $op = $_GET['op'];
+    switch ($op)
+    {
+        case "usuarios_activacion":
+            INTEFAZ__ACTIVACION_USUARIOS();
+        break;
+        default:
+            echo "ERROR: Interfaz '$op' no implementada";
+    }
+}
+
+function INTEFAZ__ACTIVACION_USUARIOS()
+{
+    $c = "SELECT * FROM ventas_usuarios WHERE estado="._N_esp_activacion;
+    $r = db_consultar($c);
+    echo db_ui_tabla($r);
+}
 ?>
