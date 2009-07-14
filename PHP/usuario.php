9566b8136ea61ec4ef80f12a0d88c188aad369b9
Vladimir Hidalgo <vlad@todosv.com>
2009-07-14 09:26:48 -0600

    CAMBIO: retornar (bool) false si el usuario solicitado no existe

--- PHP/usuario.php
+++ PHP/usuario.php
@@ -2,84 +2,84 @@
 $tablaUsuarios = 'ventas_usuarios';
 
 function _F_usuario_existe($usuario,$campo="usuario"){
-	global $tablaUsuarios;
-	$usuario = db_codex($usuario);
-	$resultado = db_consultar ("SELECT id_usuario FROM $tablaUsuarios where $campo='$usuario'");
+    global $tablaUsuarios;
+    $usuario = db_codex($usuario);
+    $resultado = db_consultar ("SELECT id_usuario FROM $tablaUsuarios where $campo='$usuario'");
 if ($resultado) {
-	$n_filas = mysql_num_rows($resultado);
-	if ( $n_filas == 1 ) {
-		return true;
-	} else {
-		return false;
-	}
+    $n_filas = mysql_num_rows($resultado);
+    if ( $n_filas == 1 ) {
+        return true;
+    } else {
+        return false;
+    }
 } else {
-	return false;
+    return false;
 }
 }
 function _F_usuario_eliminar(){}
 function _F_usuario_actualizar(){}
 
 function _F_usuario_datos($id_usuario){
-	global $tablaUsuarios;
-	$c = "SELECT * FROM $tablaUsuarios WHERE id_usuario='$id_usuario'";
-	DEPURAR ($c, 0);
-	$resultado = db_consultar ($c);
-	return db_fila_a_array($resultado);
+    global $tablaUsuarios;
+    $c = "SELECT * FROM $tablaUsuarios WHERE id_usuario='$id_usuario'";
+    DEPURAR ($c, 0);
+    $resultado = db_consultar ($c);
+    return (mysql_num_rows($resultado) > 0) ? db_fila_a_array($resultado) : false;
 }
 
 function _F_usuario_agregar($datos){
-	global $tablaUsuarios;
-	if ( !_F_usuario_existe($datos['usuario']) ){
-		db_agregar_datos ($tablaUsuarios, $datos);
-		return true;
-	} else {
-		return false;
-	}
+    global $tablaUsuarios;
+    if ( !_F_usuario_existe($datos['usuario']) ){
+        db_agregar_datos ($tablaUsuarios, $datos);
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function _F_usuario_acceder($email, $clave){
-	global $tablaUsuarios;
-	$email = db_codex (trim($email));
-	$clave = md5 (trim($clave));
+    global $tablaUsuarios;
+    $email = db_codex (trim($email));
+    $clave = md5 (trim($clave));
 
-	$c = "SELECT * FROM $tablaUsuarios WHERE email='$email' AND clave='$clave' AND estado!="._N_esp_activacion;
-	DEPURAR($c,0);
-	$resultado = db_consultar ($c);
-	if ($resultado) {
-	$n_filas = mysql_num_rows ($resultado);
-	if ( $n_filas == 1 ) {
-		$_SESSION['autenticado'] = true;
-		$_SESSION['cache_datos_usuario'] = db_fila_a_array($resultado);
-		$c = "UPDATE $tablaUsuarios SET ultimo_acceso='".mysql_datetime()."' WHERE email='$email'";
-		$resultado = db_consultar ($c);
-		return 1;
-	} else {
-		unset ($_SESSION['autenticado']);
-		unset ($_SESSION['id_usuario']);
-		return -1;
-	}
-	} else {
-		unset ($_SESSION['autenticado']);
-		unset ($_SESSION['id_usuario']);
-		echo "Error general al autenticar!"."<br />";
-		return 0;
-	}
+    $c = "SELECT * FROM $tablaUsuarios WHERE email='$email' AND clave='$clave' AND estado!="._N_esp_activacion;
+    DEPURAR($c,0);
+    $resultado = db_consultar ($c);
+    if ($resultado) {
+    $n_filas = mysql_num_rows ($resultado);
+    if ( $n_filas == 1 ) {
+        $_SESSION['autenticado'] = true;
+        $_SESSION['cache_datos_usuario'] = db_fila_a_array($resultado);
+        $c = "UPDATE $tablaUsuarios SET ultimo_acceso='".mysql_datetime()."' WHERE email='$email'";
+        $resultado = db_consultar ($c);
+        return 1;
+    } else {
+        unset ($_SESSION['autenticado']);
+        unset ($_SESSION['id_usuario']);
+        return -1;
+    }
+    } else {
+        unset ($_SESSION['autenticado']);
+        unset ($_SESSION['id_usuario']);
+        echo "Error general al autenticar!"."<br />";
+        return 0;
+    }
 }
 
 function _F_usuario_cache($campo){
-	if ( array_key_exists('cache_datos_usuario', $_SESSION) ) {
-		if ( array_key_exists ($campo, $_SESSION['cache_datos_usuario']) ) {
-			return $_SESSION['cache_datos_usuario'][$campo];
-		}else{
-			return NULL;
-		}
-	}else{
-		return NULL;
-	}
+    if ( array_key_exists('cache_datos_usuario', $_SESSION) ) {
+        if ( array_key_exists ($campo, $_SESSION['cache_datos_usuario']) ) {
+            return $_SESSION['cache_datos_usuario'][$campo];
+        }else{
+            return NULL;
+        }
+    }else{
+        return NULL;
+    }
 }
 
 function _autenticado()
 {
-	return (isset($_SESSION['autenticado']) && $_SESSION['autenticado'] = true);
+    return (isset($_SESSION['autenticado']) && $_SESSION['autenticado'] = true);
 }
 ?>
