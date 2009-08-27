ae3ec595fe55060301f626cca3a6c57ba7178948
Vladimir Hidalgo <vlad@todosv.com>
2009-08-27 09:04:18 -0600

    Búsqueda avanzada parche 2/3
    Búsqueda restringida por tiempo

--- PHP/buscar.php
+++ PHP/buscar.php
@@ -25,8 +25,10 @@ function CONTENIDO_BUSCAR()
     // Construimos la parte avanzada si fué solicitada
     if($flag_busq_adv)
     {
-        // Lugares
+        $ANDs = array();
         $LUGARES = array();
+        
+        // Lugares
         if (!empty($_GET['inc_titulo'])) $LUGARES[] = 'titulo';
         if (!empty($_GET['inc_sub'])) $LUGARES[] = 'descripcion_corta';
         if (!empty($_GET['inc_desc'])) $LUGARES[] = 'descripcion';
@@ -37,10 +39,29 @@ function CONTENIDO_BUSCAR()
         $AND_match2 = (!empty($_GET['inc_etiq'])) ? sprintf("AND id_publicacion IN (SELECT id_publicacion FROM ventas_tag_uso WHERE id_tag IN (SELECT ventas_tag.id FROM ventas_tag WHERE MATCH(ventas_tag.tag) AGAINST('%s' IN BOOLEAN MODE)))",$cadenaBusq) : "";
         
         // Precios
-        $ANDs = array();
+        
         if (!empty($_GET['pmin']) && is_numeric($_GET['pmin'])) $ANDs[] = sprintf("AND precio>='%s'",db_codex($_GET['pmin']));
         if (!empty($_GET['pmax']) && is_numeric($_GET['pmax'])) $ANDs[] = sprintf("AND precio<='%s'",db_codex($_GET['pmax']));
 
+        // Horas
+        if (!empty($_GET['inc_tiempo']) && isset($_GET['tp']) && isset($_GET['tpv']) && is_numeric($_GET['tp']) && is_numeric($_GET['tpv']) )
+        {
+            // Determinamos el operador
+            switch($_GET['tp'])
+            {
+                case 3:
+                    $operacion = "((DATEDIFF(fecha_fin,CURDATE())) * 24) > '%s'"; break;
+                case 2:
+                    $operacion = "((DATEDIFF(fecha_fin,CURDATE())) * 24) < '%s'"; break;
+                case 1:
+                    $operacion = "((DATEDIFF(CURDATE(),fecha_ini)) * 24) < '%s'"; break;
+                case 0:
+                default:
+                    $operacion = "((DATEDIFF(CURDATE(),fecha_ini)) * 24) > '%s'"; break;
+            }
+            $ANDs[] = sprintf("AND $operacion",db_codex($_GET['tpv']));
+        }
+        
         $WHERE = sprintf("fecha_fin >= CURDATE() $AND_match1 $AND_match2 $AND_categoria %s",implode(" ",$ANDs));
     }
     else
@@ -81,8 +102,9 @@ function CONTENIDO_BUSCAR()
             </td>
         </tr>
     </table>
-    Precio entre $<input type="text" name="pmin" value="<?php echo (isset($_GET['pmin']) ? @$_GET['pmin'] : '0.00'); ?>"> y $<input type="text" name="pmax" value="<?php echo (isset($_GET['pmax']) ? @$_GET['pmax'] : '0.00'); ?>"> (dolares USA | USD).<br />
-    <select name="tp"><option value="0">Publicado hace no menos de</option><option value="1">Publicado hace no más de</option><option value="2">Terminando en menos de</option><option value="3">Terminando en más de</option></select><select name="tpv"><option value="1">1 hora</option><option value="2">2 horas</option><option value="6">6 horas</option><option value="12">12 horas</option><option value="24">1 día</option><option value="48">2 días</option><option value="72">3 días</option><option value="96">4 días</option><option value="120">5 días</option><option value="144">6 días</option><option value="168">7 días</option></select><br />
+    Precio entre $<input type="text" name="pmin" value="<?php echo (isset($_GET['pmin']) ? @$_GET['pmin'] : '0.00'); ?>"> y $<input type="text" name="pmax" value="<?php echo (isset($_GET['pmax']) ? @$_GET['pmax'] : '99999.00'); ?>"> (dolares USA | USD).<br />
+    <input type="checkbox" name="inc_tiempo" value="1" <?php echo (isset($_GET['inc_tiempo']) ? 'checked="checked"' : ""); ?> />
+    <?php echo ui_combobox("tp",'<option value="0">Publicado hace no menos de</option><option value="1">Publicado hace no más de</option><option value="2">Terminando en menos de</option><option value="3">Terminando en más de</option>',@$_GET['tp']); echo ui_combobox("tpv", '<option value="1">1 hora</option><option value="2">2 horas</option><option value="6">6 horas</option><option value="12">12 horas</option><option value="24">1 día</option><option value="48">2 días</option><option value="72">3 días</option><option value="96">4 días</option><option value="120">5 días</option><option value="144">6 días</option><option value="168">7 días</option>',@$_GET['tpv']); ?><br />
     <input name="ba" type="hidden" value="1" />
     <br />
     <input type="submit" value="Realizar búsqueda refinada" />
