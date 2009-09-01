77491a5129540eece0afb80d61f820fdbd301d15
Vladimir Hidalgo <vlad@todosv.com>
2009-09-01 09:48:15 -0600

    Búsqueda avanzada parche 3/3
    Soporte de discriminación por opciones y revisión general de todas las búsquedas.

--- PHP/buscar.php
+++ PHP/buscar.php
@@ -64,9 +64,25 @@ function CONTENIDO_BUSCAR()
         
         // Opciones
         //-Características del artículo
-        if (isset($_GET['f']) && is_array($_GET['f']))
-        $ANDs[] = sprintf("AND id_publicacion IN (SELECT id_publicacion FROM ventas_flags_pub WHERE tipo='%s')",implode("' AND tipo='",db_codex($_GET['f'])));
-        
+        if (isset($_GET['f']) && is_array($_GET['f']) && isset($_GET['mf']))
+        {
+        sort($_GET['f']);
+        switch ($_GET['mf'])
+        {
+        // Cualquier coincidencia
+        case 2:
+            $ANDs[] = sprintf("AND id_publicacion IN (SELECT id_publicacion FROM ventas_flags_pub WHERE id_flag IN ('%s'))", implode("','",db_codex($_GET['f'])));
+            break;
+        // Coincidencia parcial
+        case 1:
+            $ANDs[] = sprintf("AND id_publicacion IN (SELECT id_publicacion FROM ventas_flags_pub WHERE id_flag IN ('%s') GROUP BY id_publicacion HAVING COUNT(DISTINCT id_flag) = %s)", implode("','",db_codex($_GET['f'])), count($_GET['f']));
+            break;
+        // Coincidencia exacta
+        case 0:
+        default:
+            $ANDs[] = sprintf("AND id_publicacion IN (SELECT id_publicacion FROM ventas_flags_pub group BY id_publicacion HAVING GROUP_CONCAT(id_flag ORDER BY id_flag ASC) ='%s');", implode(",",db_codex($_GET['f'])));
+        }
+        }
         $WHERE = sprintf("fecha_fin >= CURDATE() $AND_match1 $AND_match2 $AND_categoria %s",implode(" ",$ANDs));
     }
     else
@@ -82,6 +98,8 @@ function CONTENIDO_BUSCAR()
     <h1>Refinar búsqueda</h1>
     <div id="buscador">
     <form action="buscar" method="get">
+    <fieldset>
+    <legend>Texto a búscar y campos de búsqueda</legend>
         <input id="busqueda" name="b" type="text" value="<?php echo @$_GET["b"]; ?>" />
         <?php echo ui_combobox("c",'<option value="">Todas las categorias</option>'.join("",ver_hijos("","")),@$_GET["c"]); ?>
         <br />
@@ -90,26 +108,32 @@ function CONTENIDO_BUSCAR()
         <input type="checkbox" name="inc_sub" value="1" <?php echo (isset($_GET['inc_sub']) || !$flag_busq_adv ? 'checked="checked"' : ""); ?> /> Sub-título
         <input type="checkbox" name="inc_desc" value="1" <?php echo (isset($_GET['inc_desc']) ? 'checked="checked"' : ""); ?> /> Descripción
         <input type="checkbox" name="inc_etiq" value="1" <?php echo (isset($_GET['inc_etiq']) || !$flag_busq_adv ? 'checked="checked"' : ""); ?> /> Etiquetas
+    </fieldset>
+    <fieldset>
+    <legend>Opciones de la publicación</legend>
     <table class="ancha limpio marginado-y col1-3">
         <tr><th><h2>Características del artículo</h2></th><th><h2>Formas de pago admitidas</h2></th><th><h2>Formas de entrega admitidas</h2><th></tr>
         <tr>
             <td>
-            <input title="Marque si desea que se muestren las publicaciones que no tengan las opciones seleccionadas" type="checkbox" name="neg_v" /> Excluir si contienen<hr />
             <?php echo db_ui_checkboxes("f[]", "ventas_flags", "id_flag", "nombrep", "descripcion",@$_GET['f'],"","tipo='venta'"); ?>
             </td>
             <td>
-            <input title="Marque si desea que se muestren las publicaciones que no tengan las opciones seleccionadas" type="checkbox" name="neg_v" /> Excluir si contienen<hr />
             <?php echo db_ui_checkboxes("f[]", "ventas_flags", "id_flag", "nombrep", "descripcion",@$_GET['f'],"","tipo='entrega'"); ?>
             </td>
             <td>
-            <input title="Marque si desea que se muestren las publicaciones que no tengan las opciones seleccionadas" type="checkbox" name="neg_v" /> Excluir si contienen<hr />
             <?php echo db_ui_checkboxes("f[]", "ventas_flags", "id_flag", "nombrep", "descripcion",@$_GET['f'],"","tipo='pago'"); ?>
             </td>
         </tr>
     </table>
+    Método: <input title="las publicaciones encontradas cumplen exactamente con todos los criterios y ninguna más o ninguno menos" name="mf" type="radio" value="0" <?php echo (isset($_GET['mf']) && ($_GET['mf'] == 0) ? 'checked="checked"' : ""); ?> > Coincidencia exacta <input title="las publicaciones encontradas contienen al menos todos los criterios seleccionados" name="mf" type="radio" value="1" <?php echo (!isset($_GET['mf']) || ($_GET['mf'] == 1) || !$flag_busq_adv ? 'checked="checked"' : ""); ?> > Coincidencia parcial <input title="las publicaciones encontradas cumplen con al menos 1 criterio" name="mf" type="radio" value="2" <?php echo (isset($_GET['mf']) && ($_GET['mf'] == 2) ? 'checked="checked"' : ""); ?> > Cualquier coincidencia
+    </fieldset>
+    <fieldset>
+    <legend>Restricciones de precio y tiempo de publicación</legend>
     Precio entre $<input type="text" name="pmin" value="<?php echo (isset($_GET['pmin']) ? @$_GET['pmin'] : '0.00'); ?>"> y $<input type="text" name="pmax" value="<?php echo (isset($_GET['pmax']) ? @$_GET['pmax'] : '99999.00'); ?>"> (dolares USA | USD).<br />
     <input type="checkbox" name="inc_tiempo" value="1" <?php echo (isset($_GET['inc_tiempo']) ? 'checked="checked"' : ""); ?> />
     <?php echo ui_combobox("tp",'<option value="0">Publicado hace no menos de</option><option value="1">Publicado hace no más de</option><option value="2">Terminando en menos de</option><option value="3">Terminando en más de</option>',@$_GET['tp']); echo ui_combobox("tpv", '<option value="1">1 hora</option><option value="2">2 horas</option><option value="6">6 horas</option><option value="12">12 horas</option><option value="24">1 día</option><option value="48">2 días</option><option value="72">3 días</option><option value="96">4 días</option><option value="120">5 días</option><option value="144">6 días</option><option value="168">7 días</option>',@$_GET['tpv']); ?><br />
+    </input>
+    </fieldset>
     <input name="ba" type="hidden" value="1" />
     <br />
     <input type="submit" value="Realizar búsqueda refinada" />
