f288332a0c8184ddebc4ac41bb18da90b59c84b6
Vladimir Hidalgo <vlad@todosv.com>
2009-10-08 17:27:14 -0600

    Rafael's GA Tracking code

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.
 </script>
 <script type="text/javascript">
 try {
-var pageTracker = _gat._getTracker("UA-10926809-1");
+var pageTracker = _gat._getTracker("UA-611766-3");
 pageTracker._trackPageview();
 } catch(err) {}</script>
 <script type="text/javascript">
