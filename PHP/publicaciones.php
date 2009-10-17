95302ba8735f75043057a7a5b86eda036bf44824
Vladimir Hidalgo <vlad@todosv.com>
2009-10-17 06:33:35 -0600

    Arreglo en Twitter

--- PHP/publicaciones.php
+++ PHP/publicaciones.php
@@ -6,11 +6,15 @@ function Publicacion_Aprobar($id_publicacion)
     $DiasDeVigencia = db_obtener('ventas_usuarios','nDiasVigencia',"id_usuario=$id_usuario");
     $c = "UPDATE ventas_publicaciones SET tipo="._A_aceptado.", fecha_fin=date_add(CURDATE(), INTERVAL $DiasDeVigencia DAY) WHERE id_publicacion='$id_publicacion' AND id_usuario='$id_usuario' LIMIT 1";
     $r = db_consultar($c);
-    if (db_afectados() > 0)
+    $db_afectados_buffer = db_afectados();
+    if ($db_afectados_buffer > 0)
     {
         require_once('PHP/anunciadores.php');
-        tweet('Nueva publicacion: '.db_obtener('ventas_publicaciones','titulo',"id_publicacion=$id_publicacion"));
+        $c = "SELECT id_publicacion, titulo FROM ventas_publicaciones WHERE id_publicacion=$id_publicacion";
+        $r = db_consultar($c);
+        $f = mysql_fetch_assoc($r);
+        tweet('Nueva publicacion: '.$f['titulo'].' | http://www.yomachete.com/publicacion_'.$f['id_publicacion']."_".SEO($f['titulo']));
     }
-    return @db_afectados();
+    return $db_afectados_buffer;
 }
 ?>
